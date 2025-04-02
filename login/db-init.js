7a0d726f88f0b2c1bc0e74eb7967f7c13be2bdca
nelsonnedes <62315514+nelsonnedes@users.noreply.github.com>
2025-04-02 15:09:17 -0300

    Correções adicionais para o GitHub Pages

---
+++ login/db-init.js
@@ -0,0 +1,7 @@
+/**
+ * Re-exporta as funções de inicialização do banco de dados
+ * Este arquivo é usado apenas para simplificar as importações na página de login
+ */
+
+// Importa e re-exporta de ../js/db-init.js
+export * from '../js/db-init.js'; 
---
+++ login/firebase-config.js
@@ -0,0 +1,7 @@
+/**
+ * Re-exporta a configuração do Firebase do diretório correto
+ * Este arquivo é usado apenas para simplificar as importações na página de login
+ */
+
+// Importa e re-exporta de ../js/firebase-config.js
+export * from '../js/firebase-config.js'; 
