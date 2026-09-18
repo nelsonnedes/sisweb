0c6b3484ab6092a84da0284da34f96fa56939563
Nelson <62315514+nelsonnedes@users.noreply.github.com>
2026-09-18 01:53:04 -0300

    fix(mobile): paginacao fixa nos 5 modais via override html body #id (1,1,2)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026-09-17-mobile-data-fix-v15';
+const APP_VERSION = '2026-09-17-mobile-data-fix-v16';
 const CACHE_NAME = `sisweb-runtime-${APP_VERSION}`;
 const PRECACHE_URLS = [
   '/manifest.json',
